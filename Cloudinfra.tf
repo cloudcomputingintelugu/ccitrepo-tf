ad14ba7afac3f86ffc38811345648e9c1f23dc44
Naga Srinivasa <cloudcomputingchannel@gmail.com>
2025-05-10 21:13:09 +0530

    Update Cloudinfra.tf

--- Cloudinfra.tf
+++ Cloudinfra.tf
@@ -1,14 +1,7 @@
 provider "aws"{
     region = "ap-south-2"
 }
-/*
+
 resource "aws_s3_bucket" "bucket" {
   bucket = "ccitapr2025bucket"
   }
-*/
-
-resource "aws_instance" "ccitinst"{
-ami = "ami-0f48205ec8e2235e8"
-instance_type = "t3.micro"
-count = 3
-}
